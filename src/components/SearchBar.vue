12d86f93121a86c479b5b73dfce7a388f1e74724
Jonathan Stahl <jonathanjstahl7@gmail.com>
2024-10-13 18:56:17 +1100

    yes

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+export default {
+  name: 'SearchBar'
+}
+</script>
+
+<template>
+  <div id="controls">
+    <button onclick="sortImagesByColorCategory('red')">Sort by Red</button>
+    <button onclick="sortImagesByColorCategory('blue')">Sort by Blue</button>
+    <button onclick="sortImagesByColorCategory('yellow')">Sort by Yellow</button>
+    <button onclick="sortImagesByColorCategory('green')">Sort by Green</button>
+    <button onclick="sortImagesByColorCategory('purple')">Sort by Purple</button>
+    <button onclick="sortImagesByColorCategory('orange')">Sort by Orange</button>
+    <button onclick="sortImagesByColorCategory('white')">Sort by White</button>
+    <button onclick="sortImagesByColorCategory('black')">Sort by Black</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  button {
+    margin: 10px;
+    padding: 10px;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import SearchBar from './components/SearchBar.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.component('SearchBar', SearchBar)
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
